14078f577cba693b333c06b20f13b5a6af971a18
anjal <anjal@LAPTOP-2AA4CG73>
2025-01-18 21:54:25 +0530

    TL change on sum result

--- GITProj01/src/main/java/com/nt/main/App.java
+++ GITProj01/src/main/java/com/nt/main/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main(String[] args)
     {
     	App app=new App();
-        System.out.println("Sum is::"+app.sum(10,20));
+        System.out.println("Sum Amount is::"+app.sum(10,20));
     }
 }
