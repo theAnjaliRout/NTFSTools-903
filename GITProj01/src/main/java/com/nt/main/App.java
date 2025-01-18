706644ae31c2e2c0508129f34c973c150ad4f7c5
anjal <anjal@LAPTOP-2AA4CG73>
2025-01-18 11:50:46 +0530

    task1 by Dev1

--- GITProj01/src/main/java/com/nt/main/App.java
+++ GITProj01/src/main/java/com/nt/main/App.java
@@ -1,10 +1,14 @@
 package com.nt.main;
 
-/**
- * Hello world!
- */
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+public class App 
+{
+	public int sum(int x,int y)
+	{
+		return x+y;
+	}
+    public static void main(String[] args)
+    {
+    	App app=new App();
+        System.out.println("Sum is::"+app.sum(10,20));
     }
 }
--- GITProj01/src/test/java/com/nt/main/AppTest.java
+++ GITProj01/src/test/java/com/nt/main/AppTest.java
@@ -1,19 +1,36 @@
 package com.nt.main;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest {
-
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest 
+{
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    public void testSumWithPositive() 
+    {
+       App app=new App();
+       int excepted=300;
+       int actual=app.sum(100,200);
+       assertEquals(excepted,actual);
+    }
+    
+    @Test
+    public void testSumWithNegatives() 
+    {
+       App app=new App();
+       int excepted=-300;
+       int actual=app.sum(-100,-200);
+       assertEquals(excepted,actual);
+    }
+    
+    @Test
+    public void testSumWithMixedValues() 
+    {
+       App app=new App();
+       int excepted=100;
+       int actual=app.sum(-100,200);
+       assertEquals(excepted,actual);
     }
 }
