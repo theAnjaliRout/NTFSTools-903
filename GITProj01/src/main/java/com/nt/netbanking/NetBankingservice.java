abdd6309ef7de60b632ae832d83a7e820f3adb97
anjal <anjal@LAPTOP-2AA4CG73>
2025-01-21 17:56:36 +0530

    TL-- branch 2 enhancement

--- GITProj01/src/main/java/com/nt/netbanking/NetBankingservice.java
+++ GITProj01/src/main/java/com/nt/netbanking/NetBankingservice.java
@@ -8,5 +8,15 @@ public class NetBankingservice
 	{
 		return "Account is Opened with acc numner::"+new Random().nextLong(50000000000L);
 	}
+	
+	public String withdraw(long acno,double amount)
+	{
+		return amount+" is withdrawn from the account number::"+acno;
+	}
+	
+	public String deposite(long acno,double amount)
+	{
+		return amount+" is deposited into thr account number::"+acno;
+	}
 
 }
