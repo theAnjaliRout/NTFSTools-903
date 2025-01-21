23fe8739d0f6b87ecab59e041d5f903dbeac8f5e
anjal <anjal@LAPTOP-2AA4CG73>
2025-01-21 17:52:40 +0530

    Dev1 ---acccount opening logic

--- GITProj01/src/main/java/com/nt/netbanking/NetBankingservice.java
+++ GITProj01/src/main/java/com/nt/netbanking/NetBankingservice.java
@@ -1,7 +1,12 @@
 package com.nt.netbanking;
 
+import java.util.Random;
+
 public class NetBankingservice 
 {
-	
+	public String openAccount(String aadhar,double initialamount,String type)
+	{
+		return "Account is Opened with acc numner::"+new Random().nextLong(50000000000L);
+	}
 
 }
